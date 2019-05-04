de23d4c57ba099b7a0567b8f292959b0a6ce87fa
Giovanni <mz4466@gmail.com>
2019-05-04 19:31:24 +0100

    Project Completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button ballDisplay;
+        ballDisplay = (Button) findViewById(R.id.askButton);
+
+        final ImageView eightBall =(ImageView) findViewById(R.id.image_eightBall);
+
+        final int[] ballArray = {R.drawable.ball1,
+        R.drawable.ball2,
+        R.drawable.ball3,
+        R.drawable.ball4,
+        R.drawable.ball5};
+
+        ballDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("magiceightball", "Ze button has been pressed!");
+
+                Random randomNumberGenerator = new Random();
+
+                int nuber =randomNumberGenerator.nextInt(5);
+
+                Log.d("magiceightball", "The random number is: " +nuber);
+
+                eightBall.setImageResource(ballArray[nuber]);
+            }
+        });
     }
 }
